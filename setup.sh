89f0be6ba3ca7517d6690c7173a91bb8846f07fd
ahinoamta <ahinoam.tayari@gmail.com>
2020-03-28 16:01:25 +0300

    changing "Hello World" flask application:
    1. unnecessary actions have been removed from setup.sh
    2. changing VM version to "ubuntu/bionic64"
    3. technical debt from Vagrantfile has been removed

--- setup.sh
+++ setup.sh
@@ -3,12 +3,16 @@
 echo "updating apt before installation"
 sudo apt update
 
+echo "installing python 3.7"
+sudo apt install -y python3.7 
+
 echo "installing python3-pip"
 sudo apt install -y python3-pip
 pip3 install --upgrade pip
 
 echo "installing flask"
-pip3 install flask
+#'python3 -m' was added to avoid warnings
+python3 -m pip install flask
 
 echo "running flask_init.py"
-FLASK_APP=/vagrant/flask_init.py flask run --host=0.0.0.0
+FLASK_APP=/vagrant/flask_init.py flask run --host=0.0.0.0 >> /vagrant/log.log 2>&1 &
