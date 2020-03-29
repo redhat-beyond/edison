7182386400f7645fa95f2800de2285d85c860dde
ahinoamta <ahinoam.tayari@gmail.com>
2020-03-29 17:22:54 +0300

    without upgrade pip; running flask app with python3 -m

--- setup.sh
+++ setup.sh
@@ -8,11 +8,10 @@ sudo apt install -y python3.7
 
 echo "installing python3-pip"
 sudo apt install -y python3-pip
-pip3 install --upgrade pip
 
 echo "installing flask"
-#'python3 -m' has been added to avoid warnings
-python3 -m pip install flask
+pip3 install flask
 
 echo "running flask_init.py"
-FLASK_APP=/vagrant/flask_init.py flask run --host=0.0.0.0 >> /vagrant/log.log 2>&1 &
+export FLASK_APP=/vagrant/flask_init.py
+python3 -m flask run --host=0.0.0.0 >> /vagrant/log.log 2>&1 &
