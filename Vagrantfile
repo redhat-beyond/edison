5bde4d52c99817576f60c5ca26ff442905ec7cb6
xdtalx <dortal14@gmail.com>
2020-04-11 19:33:06 +0300

    Added required packages to setup.sh and forwarded additional port for the Restful API

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/bionic64"
   config.vm.provision :shell, path: "setup.sh"
   config.vm.network :forwarded_port, guest: 5000, host: 5000
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
   config.vm.provider "virtualbox" do |v|
     v.gui = false
     v.name = "Edison_test"
--- setup.sh
+++ setup.sh
@@ -12,6 +12,35 @@ sudo apt install -y python3-pip
 echo "installing flask"
 pip3 install flask
 
+echo "installing flask restful"
+pip3 install flask-restful
+
+echo "installing flask jwt"
+pip3 install flask-jwt-extended
+
+echo "installing passlib"
+pip3 install passlib
+
+echo "installing flask-sqlalchemy"
+pip3 install flask-sqlalchemy
+
+echo "installing psycopg2-binary"
+pip3 install psycopg2-binary
+
+echo "installing postgresql"
+sudo apt-get install -y postgresql postgresql-contrib
+
+echo "configuring database"
+sudo su postgres <<POSTGRESQL_COMMANDS
+psql
+CREATE DATABASE edison;
+ALTER ROLE postgres WITH PASSWORD 'edison';
+POSTGRESQL_COMMANDS
+
 echo "running flask_init.py"
 export FLASK_APP=/vagrant/flask_init.py
-python3 -m flask run --host=0.0.0.0 >> /vagrant/log.log 2>&1 &
+flask run -h 0.0.0.0 -p 5000 >> /vagrant/flask_init.log 2>&1 &
+
+echo "running app.py"
+export FLASK_APP=/vagrant/backend/app.py
+flask run -h 0.0.0.0 -p 3000 >> /vagrant/app.log 2>&1 &
