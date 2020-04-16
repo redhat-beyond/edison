54500ccf2ef078e2b34ab343d28ca0f9d7be7215
xdtalx <dortal14@gmail.com>
2020-04-16 17:09:44 +0300

    Added config file

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from backend.config import get_config_object
 import os
 
 
 # Put app and db here so the entire app could import them.
 app = Flask(__name__)
+app.config.from_object(get_config_object(app.config["ENV"]))
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:edison@127.0.0.1/edison'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
