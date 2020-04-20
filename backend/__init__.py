81cb5fc93b5f047a11016b05ed284b779af3080b
xdtalx <dortal14@gmail.com>
2020-04-20 14:04:47 +0300

    Improved the load of config object

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,15 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from backend.config import get_config_object
 import os
 
 
 # Put app and db here so the entire app could import them.
 app = Flask(__name__)
-
-if app.config["ENV"] == "production":
-    app.config.from_object("backend.config.ProductionConfig")
-else:
-    app.config.from_object("backend.config.DevelopmentConfig")
-    
+app.config.from_object(get_config_object(app.config["ENV"]))
 basedir = os.path.abspath(os.path.dirname(__file__))
 db = SQLAlchemy(app)
--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,23 @@
 import secrets
+import importlib, inspect
+import sys, inspect
 
+config_dict = {}
 
-class Config(object):
+# If config_dict is empty this function builds it dynamically 
+# and returns the appropriate config object path.
+def get_config_object(env_keyword: str):
+    if(len(config_dict) == 0):
+        # Iterating through all config.py members
+        for name, obj in inspect.getmembers(sys.modules[__name__]):
+            # We're interested only with the derived classes of the Config class
+            if inspect.isclass(obj) and name != "Config":
+                config_dict[obj.ENV_KEYWORD] = ".".join([obj.__module__, name])
+    
+    return config_dict[env_keyword]
+    
+class Config:
+    ENV_KEYWORD = ""
     DEBUG = False
     # Turn off the Flask-SQLAlchemy event system
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -16,7 +32,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:edison@127.0.0.1/edison'
 
 class ProductionConfig(Config):
+    ENV_KEYWORD = "production"
     pass
 
 class DevelopmentConfig(Config):
+    ENV_KEYWORD = "development"
     DEBUG = True
