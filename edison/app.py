2f2d231810fa1af20ca600360769adbb10523c99
xdtalx <dortal14@gmail.com>
2020-04-21 20:36:17 +0300

    Added use of Flask-Migrate extension
    All files inside edison/migrations were generated automatically by the extension.

--- edison/app.py
+++ edison/app.py
@@ -3,13 +3,15 @@ import edison
 from flask import render_template
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 
 # API description in swagger - https://app.swaggerhub.com/apis/DoRTaL94/UserManagment/1.0.0
 
 app = edison.app
+db = edison.db
+migrate = Migrate(app, db)
 # Creates all tables in app. Table's name defined with __tablename__ variable.
-edison.db.create_all()
 api = Api(app)
 # Creation of Json-Web-Token manager.
 # In order to reach secured endpoints client should add an authorization header with the value Bearer <token>.
