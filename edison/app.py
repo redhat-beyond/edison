48d0ed6b402909e2e35038ebd0486b81734e23a5
naamaCohen1 <naamacohen26@gmail.com>
2020-04-23 13:00:35 +0300

    changing the run from home page

--- edison/app.py
+++ edison/app.py
@@ -7,7 +7,7 @@ app = edison.app
 
 @app.route("/")
 def index():
-	return render_template('main.html')
+	return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
