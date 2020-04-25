488aa7555c721f09dd392283c7d67ee8a6e2bf71
dortal94 <dortal14@gmail.com>
2020-04-25 19:31:31 +0100

    Added jwt support
    - Added a callback that checks if JWT is blacklisted.
    Token should be inside the token_blacklist table if it is used for logout.
    - Added jwt configurations inside config file.

--- edison/config.py
+++ edison/config.py
@@ -19,6 +19,14 @@ def get_config_object(env_keyword: str):
 class Config:
     ENV_KEYWORD = ""
     DEBUG = False
+    # Enables response message for unauthenticated requests
+    PROPAGATE_EXCEPTIONS = True
+    # This tells the JWTManager to use jwt.token_in_blacklist_loader callback
+    JWT_BLACKLIST_ENABLED = True
+    # JWTManager uses this secret key for creating tokens
+    JWT_SECRET_KEY = secrets.token_hex(24)
+    # We're going to check if both access_token and refresh_token are black listed
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
     # Turns off the Flask-SQLAlchemy event system
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:edison@127.0.0.1/edison'
