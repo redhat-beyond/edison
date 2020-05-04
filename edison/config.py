4159b6814cf613f00eba10e0de05ac8987b1957d
Roni Rushkin <roni.rushkin@gmail.com>
2020-05-04 15:15:37 +0300

    fix indentation in docstring

--- edison/config.py
+++ edison/config.py
@@ -3,21 +3,20 @@ import sys
 
 
 def get_config_object(env_keyword: str):
-"""
-Returns the the desired config class path.
+    """
+    Returns the the desired config class path.
 
-The function iterates through a dictionary returned by inspect.
-The dictionary contains details about all of the file members.
-Its key is the name of the member and value is obj which contains all the details about the member.
-The desired config path is being picked by the ENV_KEYWORD field defined in the config class.
+    The function iterates through a dictionary returned by inspect.
+    The dictionary contains details about all of the file members.
+    Its key is the name of the member and value is obj which contains all the details about the member.
+    The desired config path is being picked by the ENV_KEYWORD field defined in the config class.
 
-Parameters:
-env_keyword (str): Should be equals to one of the config classes ENV_KEYWORD field.
+    Parameters:
+    env_keyword (str): Should be equals to one of the config classes ENV_KEYWORD field.
 
-Returns:
-str: module_name.class_name, which is the full path of the config class.
-"""
-
+    Returns:
+    str: module_name.class_name, which is the full path of the config class.
+    """
     for name, obj in inspect.getmembers(sys.modules[__name__]):
         if inspect.isclass(Config) and obj.ENV_KEYWORD == env_keyword:
             return ".".join([obj.__module__, name])
