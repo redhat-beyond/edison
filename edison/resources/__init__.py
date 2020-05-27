35c313449d50e64d077dd07a37de3fe40dd6e967
omri <omrico94@gmail.com>
2020-05-27 18:50:21 +0300

    separate from user managments pr's

--- edison/resources/__init__.py
+++ edison/resources/__init__.py
@@ -1,8 +1,3 @@
 from .user import User
-from .users import Users
-from .login import Login
-from .signup import SignUp
-from .logout import (LogoutAccess, LogoutRefresh)
-from .token_refresh import TokenRefresh
 from .policy import Policy
 from .policies import Policies
